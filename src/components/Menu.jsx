e11a638cb89583144788de4788ca3c5eca31338e
joshikannan <joshikannan99@gmail.com>
2025-04-17 18:44:19 +0530

    comments menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -59,23 +59,23 @@ const Hr = styled.hr`
   border: 0.5px solid ${({ theme }) => theme.soft};
 `;
 
-const Login = styled.div`
-  font-size: 10px;
-  color: #aaaaaa;
-`;
-const Button = styled.button`
-  padding: 5px 15px;
-  background-color: transparent;
-  border: 1px solid #3ea6ff;
-  color: #3ea6ff;
-  border-radius: 3px;
-  font-weight: 500;
-  margin-top: 10px;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  gap: 5px;
-`;
+// const Login = styled.div`
+//   font-size: 10px;
+//   color: #aaaaaa;
+// `;
+// const Button = styled.button`
+//   padding: 5px 15px;
+//   background-color: transparent;
+//   border: 1px solid #3ea6ff;
+//   color: #3ea6ff;
+//   border-radius: 3px;
+//   font-weight: 500;
+//   margin-top: 10px;
+//   cursor: pointer;
+//   display: flex;
+//   align-items: center;
+//   gap: 5px;
+// `;
 
 const Title = styled.h2`
   font-size: 12px;
